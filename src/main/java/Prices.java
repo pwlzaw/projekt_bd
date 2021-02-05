b791d4fab11420df25f12dfe16f29e7c13275227
pwlzaw <pwlzawadka@gmail.com>
2021-02-05 20:16:40 +0100

    commit 3

---
+++ src/main/java/Prices.java
@@ -0,0 +1,38 @@
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Prices {
+    private IntegerProperty cena;
+    private StringProperty rozmiar;
+
+    public Prices() {
+        cena = new SimpleIntegerProperty();
+        rozmiar = new SimpleStringProperty();
+    }
+
+    public int getCena() {
+        return cena.get();
+    }
+
+    public IntegerProperty cenaProperty() {
+        return cena;
+    }
+
+    public void setCena(int cena) {
+        this.cena.set(cena);
+    }
+
+    public String getRozmiar() {
+        return rozmiar.get();
+    }
+
+    public StringProperty rozmiarProperty() {
+        return rozmiar;
+    }
+
+    public void setRozmiar(String rozmiar) {
+        this.rozmiar.set(rozmiar);
+    }
+}
