d7b4b9f26fd0452f1a00e2b7cb455fc8a2aa34bb
MisterLious <53485116+MisterLious@users.noreply.github.com>
2021-02-06 18:09:19 +0100

    controlery i javafx

--- src/main/java/UserViewController.java
+++ src/main/java/UserViewController.java
@@ -1,2 +1,54 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class UserViewController {
+
+    @FXML
+    private Button buttonHistory;
+
+    @FXML
+    private Button buttonState;
+
+    @FXML
+    private TableView<?> tableState;
+
+    @FXML
+    private TableColumn<?, ?> tableStateColumnId;
+
+    @FXML
+    private TableColumn<?, ?> tableStateColumnState;
+
+    @FXML
+    private TableColumn<?, ?> tableStateColumnReciver;
+
+    @FXML
+    private TableColumn<?, ?> tableStateColumnAdres;
+
+    @FXML
+    private TableColumn<?, ?> tableHistoryColumnId;
+
+    @FXML
+    private TableColumn<?, ?> tableHistoryColumnState;
+
+    @FXML
+    private TableColumn<?, ?> tableHistoryColumnReciver;
+
+    @FXML
+    private TableColumn<?, ?> tableHistoryColumnSendDate;
+
+    @FXML
+    private TableColumn<?, ?> tableHistoryColumnReciveDate;
+
+    @FXML
+    private TextField txtReciver;
+
+    @FXML
+    private TextField txtMachineID;
+
+    @FXML
+    private Button buttonSendPackage;
+
 }
