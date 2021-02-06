bf831b82530ee7e6329737f23276d52b0c1e8841
MisterLious <53485116+MisterLious@users.noreply.github.com>
2021-02-06 19:16:00 +0100

    controlery i javafx

--- src/main/java/ModeratorViewController.java
+++ src/main/java/ModeratorViewController.java
@@ -1,2 +1,59 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class ModeratorViewController {
+
+    @FXML
+    private Button buttonStats;
+
+    @FXML
+    private Button buttonToBeDelivered;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private TextField textAutomatId;
+
+    @FXML
+    private Button buttonRecive;
+
+    @FXML
+    private Button buttonDeliver;
+
+    @FXML
+    private TextField textPackageId;
+
+    @FXML
+    private Button buttonDayEarnings;
+
+    @FXML
+    private Text textOutput;
+
+    @FXML
+    private TableView<?> tableStats;
+
+    @FXML
+    private TableColumn<?, ?> tableStatsId;
+
+    @FXML
+    private TableColumn<?, ?> tableStatsAdres;
+
+    @FXML
+    private TableColumn<?, ?> tableStatsDeliveredNumber;
+
+    @FXML
+    private TableView<?> tableToBeDelivered;
+
+    @FXML
+    private TableColumn<?, ?> tableToBeDeliveredId;
+
+    @FXML
+    private TableColumn<?, ?> tableToBeDeliveredAdres;
+
 }
